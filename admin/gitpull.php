84f9f2080eb486e74709e1fa48127715b26be952
WWW daemon apache <wwwrun@gigantor.dnsdhcp.provo.novell.com>
2013-06-14 12:55:40 -0600

    Test Commit

--- admin/gitpull.php
+++ admin/gitpull.php
@@ -6,9 +6,17 @@
 	if(in_array($current_user, $authorized_users)) {
 		if(isset($_POST['pull'])) {
 			pullRevision($current_user);
-		} else if (isset($_POST['pushChanges'])) {
+		} else if (isset($_POST['pushChanges']) && $_POST['pushChanges'] != '') {
 			pushRevision($_POST['pushChanges'], $current_user);
-		}else {
+		} else if (isset($_POST['pushChanges']) && $_POST['pushChanges'] == '') {
+			echo "You must enter a commit message...";
+			echo "<form action='gitpull.php' method='POST'>";
+				echo "<h4>Please enter a message for your commit: </h4>";
+				echo "<input type='text' size='50' name='pushChanges' id='pushChanges' value=''></input>";
+				echo "<button type='submit'>Push Changes</button><br/><br/>";
+				echo "<button name='pull' type='submit'>Pull Changes</button>";
+			echo "</form>";
+		} else {
 			echo "<form action='gitpull.php' method='POST'>";
 				echo "<h4>Please enter a message for your commit: </h4>";
 				echo "<input type='text' size='50' name='pushChanges' id='pushChanges' value=''></input>";
@@ -26,6 +34,7 @@
 		chdir($path);
 		echo "<h4>Performing: 'git pull' as user: " . $current_user . "</h4>";
 		echo "<h5>" . shell_exec("git pull -v 2>&1") . "</h5>";
+		error_log('Performed git pull as user: ' . $current_user);
 	}
 	
 	function pushRevision($message, $current_user) {
@@ -35,5 +44,6 @@
 		echo "<h5>" . shell_exec("git add . -v 2>&1") . "</h5>";
 		echo "<h5>" . shell_exec("git commit -m '" . $message . "' -v 2>&1") . "</h5>";
 		echo "<h5>" . shell_exec("git push -v 2>&1") . "</h5>";
+		error_log('Performed git pull as user: ' . $current_user);
 	}
 ?>
