d911877264edfd1075f4d77d52a16b734ee1ef0d
WWW daemon apache <wwwrun@gigantor.dnsdhcp.provo.novell.com>
2013-06-14 12:47:02 -0600

    Added Admin Menu

---
+++ admin/authorizedGitUsers.php
@@ -0,0 +1,7 @@
+<?php
+	/* This is a list of users authorized to perform git pulls on www.opensuse.org
+	   All usernames should be listed in lowercase */
+	$authorized_users = array(
+		'scweber'
+	);
+?>
